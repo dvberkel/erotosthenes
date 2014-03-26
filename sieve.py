26257da904b715cc665a568154e8a6f27e4b6101
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-03-26 08:27:20 +0100

    abstracted a sieve step

--- sieve.py
+++ sieve.py
@@ -3,14 +3,17 @@
 def numbers(upper_bound):
     return [ (n, []) for n in range(1, upper_bound) ]
 
-if __name__ == '__main__':
-    numbers = numbers(10)
-    numbers[0][1].append(0)
-    current_index = 1
+def sieve_step(numbers, current_index):
     d = numbers[current_index][0]
     index = 2
     while d * index - 1 < len(numbers):
         numbers[d * index - 1][1].append(d)
         index += 1
+
+if __name__ == '__main__':
+    numbers = numbers(10)
+    numbers[0][1].append(0)
+    current_index = 1
+    sieve_step(numbers, current_index)
 
     print numbers
