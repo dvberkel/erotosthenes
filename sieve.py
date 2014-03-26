049eaf58930679221d7d58231d995b520360cf4c
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-03-26 08:25:42 +0100

    sieve first prime

--- sieve.py
+++ sieve.py
@@ -6,4 +6,11 @@ def numbers(upper_bound):
 if __name__ == '__main__':
     numbers = numbers(10)
     numbers[0][1].append(0)
+    current_index = 1
+    d = numbers[current_index][0]
+    index = 2
+    while d * index - 1 < len(numbers):
+        numbers[d * index - 1][1].append(d)
+        index += 1
+
     print numbers
