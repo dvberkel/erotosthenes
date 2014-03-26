b12e948478f646f9b4ae93a14496c31e7a66d6e5
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-03-26 08:30:07 +0100

    determine next current index

--- sieve.py
+++ sieve.py
@@ -10,10 +10,19 @@ def sieve_step(numbers, current_index):
         numbers[d * index - 1][1].append(d)
         index += 1
 
+def next_current_index(numbers, current_index):
+    current_index += 1
+    while (len(numbers[current_index][1]) > 0):
+        current_index += 1
+    return current_index
+
+
 if __name__ == '__main__':
     numbers = numbers(10)
     numbers[0][1].append(0)
     current_index = 1
     sieve_step(numbers, current_index)
+    current_index = next_current_index(numbers, current_index)
 
+    print current_index
     print numbers
